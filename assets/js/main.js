fe5f77d2cb7cd453f288c420ab5410718659801c
JHernanLS <jhernan.lopez92@gmail.com>
2022-10-10 14:04:37 -0500

    Segunda edicion del carrito sin funcionalidad aun

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,8 +57,8 @@ const themeIcon = document.getElementById( "theme-btn" )
 const cart = document.getElementById("cart-container")
 const shopIcon = document.getElementById("cart-shop")
 const shopCloseIcon = document.getElementById("close--cart")
-//agregar y quitar la clase hide para cerrar y abrir carrito 
 
+//agregar y quitar la clase hide para cerrar y abrir carrito 
 shopIcon.addEventListener("click", () => {
   cart.classList.remove("hide") 
 })
@@ -66,8 +66,43 @@ shopCloseIcon.addEventListener( "click", () => {
   cart.classList.add("hide")
 })
 
+
+/* ----------------------MOSTRAR LISTADO DE PRODUCTOS--------------------------- */
+// contenedor.innerHTML = "html"
+const showProducts = () => {
+  const productContainer = document.getElementById("products-list")
+
+  let fragment = ``
+
+  items.forEach( producto => {
+      fragment += `
+      <div class="product-card" id="${producto.id}">
+         <div class="div-img-product">
+            <img src="${producto.image}" alt="" class="img-product">
+            <i class='bx bx-plus-circle' ></i>
+          </div>
+          <div clas="info-product">
+            <p>${producto.price}" | Stock:" ${producto.quantity}</p>
+            <p>${producto.name}</p>
+          </div>
+      </div>
+      <br>
+       `
+  })
+
+  productContainer.innerHTML = fragment
+
+
+}
+
+
+
+
+
 /**Esta funcion ejecuta las funciones al cargar la pagina */
 document.addEventListener( "DOMContentLoaded", () =>{
   loadComponent()  //imagen cargando
- 
+  showProducts()
 })
+
+
